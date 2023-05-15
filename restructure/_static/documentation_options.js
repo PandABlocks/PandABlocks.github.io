e1d0a3ad70980559405577405045196c663b155c
tomtrafford <tomtrafford@users.noreply.github.com>
2023-05-15 11:03:46 +0000

    deploy: bff65af6d09f0ab9cf6428e9676c941f8a9a4228

--- restructure/_static/documentation_options.js
+++ restructure/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev64+gc920db7',
+    VERSION: '0.1.dev64+gbff65af',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
