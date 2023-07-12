2b8172d5b0f8fac0ef34e5f032bc321a35d38e4d
tomtrafford <tomtrafford@users.noreply.github.com>
2023-07-12 08:44:03 +0000

    deploy: 9c8cb2635ed718a2b7d0f8a70fc092c1d4647134

--- jitter_docs/_static/documentation_options.js
+++ jitter_docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev67+g22acba5',
+    VERSION: '0.1.dev67+g9c8cb26',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
